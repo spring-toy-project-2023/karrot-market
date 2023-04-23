7b4517df90e5b04d8d2662ef2106a5306065d9aa
tae0y <tae0yp@gmail.com>
2023-04-23 10:23:00 +0900

    [karrot-0013] TEST DB H2 > docker compose 변경 #24

---
+++ src/test/com/karrot/TestContainer.java
@@ -0,0 +1,19 @@
+package com.karrot;
+
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.DockerComposeContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.io.File;
+
+@Testcontainers
+@ActiveProfiles("test")
+public abstract class TestContainer {
+    public static final DockerComposeContainer composeContainer;
+
+    static
+    {
+        composeContainer = new DockerComposeContainer(new File("src/test/resources/docker-compose-test.yml"));
+        composeContainer.start();
+    }
+}
