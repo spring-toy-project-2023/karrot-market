a8645d129a6d86022a7999c1407ad8285f885882
tae0y <tae0yp@gmail.com>
2023-04-28 19:57:47 +0900

    pr test

--- src/main/java/com/karrot/App.java
+++ src/main/java/com/karrot/App.java
@@ -3,6 +3,7 @@ package com.karrot;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 
+//PULL REQUEST TEST
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
